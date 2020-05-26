f2a86ca164c2a046af1e51b2d10fc19c90e5a998
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-26 11:06:08 -0600

    Set up basic structure of Vertex

--- src/main/scala/tropicalcurves/puregraphs/Vertex.scala
+++ src/main/scala/tropicalcurves/puregraphs/Vertex.scala
@@ -1,5 +1,12 @@
 package tropicalcurves.puregraphs
 
-class Vertex {
+// Simple data holder where equality is based on reference
+class Vertex[A](val data: A)
 
+object Vertex{
+  // Can initialize like a case class
+  def apply[A](data: A): Vertex[A] = new Vertex(data)
+
+  // Can pattern match like a case class
+  def unapply[A](arg: Vertex[A]): Option[A] = Some(arg.data)
 }
