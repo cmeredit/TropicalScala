0281bf175502565569e03e5b70bfbde7b4634384
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-26 21:15:27 -0600

    Add basic Tropical Curve structure

---
+++ src/main/scala/tropicalcurves/TropicalCurve.scala
@@ -0,0 +1,23 @@
+package tropicalcurves
+
+import tropicalcurves.puregraphs._
+
+class TropicalCurve[B](adjacency: Map[Vertex[Int], Set[(Vertex[Int], B)]], legs: Set[Leg[Int]]) extends Graph[Int, B](adjacency, legs) {
+
+  val core: TropicalCurve[B] = {
+    val leavesToPrune = vertices.filter(degree(_) < 2).filter(_.data == 0)
+    if (leavesToPrune.isEmpty) {
+      this
+    } else {
+      import TropicalCurve.realizeAsCurve
+      filterOutVertices(leavesToPrune).core
+    }
+  }
+
+}
+
+object TropicalCurve {
+  implicit def realizeAsCurve[B](g: Graph[Int, B]): TropicalCurve[B] = {
+    new TropicalCurve[B](g.adjacency, g.legs)
+  }
+}
