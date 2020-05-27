7e6d470c69fcb66a77aea64d9a72ee9372eff639
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-26 23:06:23 -0600

    Set up basic iso check structure

--- src/main/scala/tropicalcurves/graphiso/GraphIso.scala
+++ src/main/scala/tropicalcurves/graphiso/GraphIso.scala
@@ -1,5 +1,15 @@
 package tropicalcurves.graphiso
+import tropicalcurves.puregraphs._
 
 object GraphIso {
+
+  def graphsAreIsomorphic[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = {
+    if (g.numVertices != h.numVertices) false
+    else if (g.numEdges != h.numEdges) false
+    else if (g.numLegs != h.numLegs) false
+    else checkBruteForceForIsomorphism(g, h)
+  }
+
+  private def checkBruteForceForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = ???
 
 }
