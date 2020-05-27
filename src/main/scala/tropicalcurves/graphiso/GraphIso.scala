03862421490294c5d135e71cd63301f2d4eca7fc
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-26 23:20:30 -0600

    Remove redundant heuristics

--- src/main/scala/tropicalcurves/graphiso/GraphIso.scala
+++ src/main/scala/tropicalcurves/graphiso/GraphIso.scala
@@ -4,14 +4,11 @@ import tropicalcurves.puregraphs._
 object GraphIso {
 
   def graphsAreIsomorphic[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = {
-    if (g.numVertices != h.numVertices) false
-    else if (g.numEdges != h.numEdges) false
-    else if (g.numLegs != h.numLegs) false
-    else if (g.spanningForest.size != h.spanningForest.size) false
+    if (g.spanningForest.size != h.spanningForest.size) false
     else if (g.numVerticesWithCharacteristic != h.numVerticesWithCharacteristic) false
     else checkBruteForceForIsomorphism(g, h)
   }
 
-  private def checkBruteForceForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = ???
+  private def checkBruteForceForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = true
 
 }
