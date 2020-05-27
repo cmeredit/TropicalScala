97d2d43c424305b43de858ffdc77e2fa1ee0d557
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-27 09:06:29 -0600

    Add check for whether or not a bijection is an isomorphism

--- src/main/scala/tropicalcurves/graphiso/GraphIso.scala
+++ src/main/scala/tropicalcurves/graphiso/GraphIso.scala
@@ -25,6 +25,41 @@ object GraphIso {
     }
   }
 
+  private def conditionHoldsForAll[A](collection: Iterable[A])(condition: A => Boolean): Boolean = {
+    collection.foldLeft(true)((holdsSoFar, nextItem) => {
+      if (holdsSoFar) condition(nextItem)
+      else false
+    })
+  }
+
+  def checkIfBijectionPreservesEdges[A, B](domain: Graph[A, B], codomain: Graph[A, B],
+                                           bij: Map[Vertex[A], Vertex[A]]): Boolean = {
+    val domainVertexPairs: Set[(Vertex[A], Vertex[A])] = for (v <- domain.vertices;
+                                                              w <- domain.vertices) yield (v, w)
+    conditionHoldsForAll(domainVertexPairs)(vertexPair => {
+      val (v, w) = vertexPair
+      val numInputEdges: Int = domain.adjacency(v).count(_._1 == w)
+      val numOutputEdges: Int = codomain.adjacency(bij(v)).count(_._1 == bij(w))
+      numInputEdges == numOutputEdges
+    })
+  }
+
+  def checkIfBijectionPreservesData[A, B](domain: Graph[A, B], bij: Map[Vertex[A], Vertex[A]]): Boolean = {
+    conditionHoldsForAll(domain.vertices)(v => v.data == bij(v).data)
+  }
+
+  def checkIfBijectionPreservesLegs[A, B](domain: Graph[A, B], codomain: Graph[A, B],
+                                          bij: Map[Vertex[A], Vertex[A]]): Boolean = {
+    conditionHoldsForAll(domain.vertices)(v => domain.legDegree(v) == codomain.legDegree(bij(v)))
+  }
+
+  def checkIfBijectionIsIsomorphism[A, B](domain: Graph[A, B], codomain: Graph[A, B],
+                                          bij: Map[Vertex[A], Vertex[A]]): Boolean = {
+    checkIfBijectionPreservesEdges(domain, codomain, bij) &&
+      checkIfBijectionPreservesData(domain, bij) &&
+      checkIfBijectionPreservesLegs(domain, codomain, bij)
+  }
+
   private def checkBruteForceForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = true
 
 }
