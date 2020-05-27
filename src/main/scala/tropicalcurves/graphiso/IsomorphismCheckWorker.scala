f5e4a6a18443539aed4bfbf200c35dae226c7836
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-27 07:52:54 -0600

    Set down basic actor structure

--- src/main/scala/tropicalcurves/graphiso/IsomorphismCheckWorker.scala
+++ src/main/scala/tropicalcurves/graphiso/IsomorphismCheckWorker.scala
@@ -1,5 +1,17 @@
 package tropicalcurves.graphiso
 
-class IsomorphismCheckWorker {
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+
+object IsomorphismCheckWorker {
+  def apply(): Behavior[IsoMessage] =
+    Behaviors.setup[IsoMessage](context => new IsomorphismCheckWorker()(context))
+}
+
+class IsomorphismCheckWorker(context: ActorContext[IsoMessage]) extends AbstractBehavior[IsoMessage](context) {
+
+  override def onMessage(msg: IsoMessage): Behavior[IsoMessage] = ???
 
 }
