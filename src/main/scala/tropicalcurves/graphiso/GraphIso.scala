54e1fd0571637dfab3fc31aa324a64a41714a65a
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-27 08:16:38 -0600

    Add a bijection generator helper

--- src/main/scala/tropicalcurves/graphiso/GraphIso.scala
+++ src/main/scala/tropicalcurves/graphiso/GraphIso.scala
@@ -9,6 +9,22 @@ object GraphIso {
     else checkBruteForceForIsomorphism(g, h)
   }
 
+  def getBijections[A, B](m: Map[A, Vector[Vector[B]]]): Vector[Map[A, Vector[B]]] = {
+    if (m.isEmpty) Vector()
+    else if (m.size == 1) {
+      val h = m.head
+      val key: A = h._1
+      val values: Vector[Vector[B]] = h._2
+      values.map(v => Map(key -> v))
+    } else {
+      val tailBijections = getBijections(m.tail)
+      val headBijections = getBijections(Map(m.head))
+      for (headBij <- headBijections;
+           tailBij <- tailBijections)
+        yield headBij ++ tailBij
+    }
+  }
+
   private def checkBruteForceForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]): Boolean = true
 
 }
