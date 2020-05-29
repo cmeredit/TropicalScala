5723d45446585d73474e2398e6b0d2892f813ee3
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-29 12:27:35 -0600

    Add GraphIso.getBijections example

--- src/main/scala/tropicalcurves/Main.scala
+++ src/main/scala/tropicalcurves/Main.scala
@@ -1,5 +1,34 @@
 package tropicalcurves
 
+import tropicalcurves.graphiso.GraphIso
+import tropicalcurves.puregraphs._
+
 object Main extends App {
-  println("Hello, world!")
+
+  val v = Vertex(0, Some("Joe"))
+  val v1 = Vertex(0, Some("v1"))
+  val v2 = Vertex(0, Some("v2"))
+  val v3 = Vertex(0, Some("v3"))
+  val v4 = Vertex(0, Some("v4"))
+  val e = new Edge(v, v)
+  val leg = new Leg(v)
+
+  val g1 = new Graph[Int, Double](Map(
+    v -> Set((v1, 1.0), (v4, 1.0)),
+    v1 -> Set((v, 1.0), (v2, 1.0), (v4, 1.0)),
+    v2 -> Set((v1, 1.0)),
+    v3 -> Set(),
+    v4 -> Set((v, 1.0), (v1, 1.0))),
+    Set(leg))
+
+  g1.spanningForest foreach println
+  println("G is " + {
+    if (g1.isConnected) "" else "not "
+  } + "connected!")
+
+  println(GraphIso.graphsAreIsomorphic(g1, g1))
+  println(GraphIso.graphsAreIsomorphic(g1.filterOutVertex(v), g1))
+
+  println(GraphIso.getBijections[Int, Int](Map(0 -> Vector(Vector(0, 1), Vector(1, 0)), 1 -> Vector(Vector(2, 3), Vector(3, 2)))))
+
 }
