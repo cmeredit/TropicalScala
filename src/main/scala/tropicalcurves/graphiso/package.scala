5b131821b777937f63121b12b8d932728cf9157f
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-29 18:27:58 -0600

    Set up more messages

--- src/main/scala/tropicalcurves/graphiso/package.scala
+++ src/main/scala/tropicalcurves/graphiso/package.scala
@@ -1,5 +1,11 @@
 package tropicalcurves
 
+import akka.actor.typed.ActorRef
+import tropicalcurves.puregraphs._
+
 package object graphiso {
   trait IsoMessage
+  final case class ReduceGraphsByIsomorphism[A, B](graphs: Vector[Graph[A, B]]) extends IsoMessage
+  final case class CheckForIsomorphism[A, B](g: Graph[A, B], h: Graph[A, B]) extends IsoMessage
+  final case class IsomorphismResult[A, B](g: Graph[A, B], h: Graph[A, B], isomorphic: Boolean) extends IsoMessage
 }
