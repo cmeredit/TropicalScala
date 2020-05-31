a997162b4ad5b524ea5942e75f919852398a951d
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-31 10:52:50 -0600

    Begin working on the partitionAdjacency function

--- src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
+++ src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
@@ -46,13 +46,39 @@ class ModuliSpace(val g: Int, val n: Int) {
                                    baseVert: Vertex[Int],
                                    newVert1: Vertex[Int],
                                    newVert2: Vertex[Int]): Vector[Map[Vertex[Int], Set[(Vertex[Int], Double)]]] = {
-      ???
+      if (adj.keySet.contains(baseVert) && adj(baseVert).nonEmpty) {
+        val vertToFlip = adj(baseVert).head
+        val updatedAdj: Map[Vertex[Int], Set[(Vertex[Int], Double)]] = adj
+          .map({ // Remove vertToFlip from adj(baseVert)
+            case (baseVert, _) => (baseVert, adj(baseVert).tail)
+            case x => x
+          })
+
+        val newAdj1: Map[Vertex[Int], Set[(Vertex[Int], Double)]] = if (updatedAdj.keySet.contains(newVert1)) {
+          updatedAdj.map({
+            case (newVert1, _) => (newVert1, updatedAdj(newVert1) + vertToFlip)
+            case x => x
+          })
+        } else updatedAdj ++ Map(newVert1 -> Set(vertToFlip))
+
+        val newAdj2: Map[Vertex[Int], Set[(Vertex[Int], Double)]] = if (updatedAdj.keySet.contains(newVert2)) {
+          updatedAdj.map({
+            case (newVert2, _) => (newVert2, updatedAdj(newVert2) + vertToFlip)
+            case x => x
+          })
+        } else updatedAdj ++ Map(newVert2 -> Set(vertToFlip))
+        
+        partitionAdjacency(newAdj1, baseVert, newVert1, newVert2) ++ partitionAdjacency(newAdj1, baseVert, newVert1, newVert2)
+
+      } else if (adj.toSet.flatMap(_._2).map(_._1).contains(baseVert)) {
+        ???
+      } else Vector()
     }
 
     def getSplittingSpecialization(g: UndirectedGraph[Int, Double], v: Vertex[Int], g1: Int, g2: Int,
                                    S: Set[Vertex[Int]], T: Set[Vertex[Int]]): Option[UndirectedGraph[Int, Double]] = {
       val canSplit = g.vertices.contains(v) && // g must have v as a vertex
-        g.adjacentVertices(v) == S ++ T && // S and T must cover the adjacent ????? of v
+        g.adjacentVertices(v).toSet == S ++ T && // S and T must cover the adjacent ????? of v
         S.intersect(T).isEmpty &&
         g1 >= 0 && g2 >=0 && v.data == g1 + g2 &&
         {if (g1 == 0) S.size >= 2 else true} &&
