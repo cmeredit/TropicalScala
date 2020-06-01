6ad0db72d569820c20c9d5f13f3066d76bb31077
Connor Meredith <connor.meredith0913@gmail.com>
2020-06-01 05:31:23 -0600

    Fix a bug in map manipulation

--- src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
+++ src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
@@ -42,38 +42,33 @@ class ModuliSpace(val g: Int, val n: Int) {
       }
     }
 
-    private def partitionAdjacency(adj: Map[Vertex[Int], Vector[(Vertex[Int], Double)]],
+    def partitionAdjacency(adj: Map[Vertex[Int], Vector[(Vertex[Int], Double)]],
                                    baseVert: Vertex[Int],
                                    newVert1: Vertex[Int],
-                                   newVert2: Vertex[Int]): Vector[Map[Vertex[Int], Set[(Vertex[Int], Double)]]] = {
+                                   newVert2: Vertex[Int]): Vector[Map[Vertex[Int], Vector[(Vertex[Int], Double)]]] = {
       type AdjMap = Map[Vertex[Int], Vector[(Vertex[Int], Double)]]
       // First handle the case where baseVert points to other vertices
       if (adj.keySet.contains(baseVert) && adj(baseVert).nonEmpty) {
         val vertToFlip = adj(baseVert).head
-        val updatedAdj: AdjMap = adj
-          .map({ // Remove vertToFlip from adj(baseVert)
-            case (baseVert, _) => (baseVert, adj(baseVert).tail)
-            case x => x
-          })
+        val updatedAdj: AdjMap = adj.transform((k, v) => if (k == baseVert) v.drop(1) else v)
 
         val newAdj1: AdjMap = if (updatedAdj.keySet.contains(newVert1)) {
-          updatedAdj.map({
-            case (newVert1, _) => (newVert1, updatedAdj(newVert1).appended(vertToFlip))
-            case x => x
-          })
-        } else updatedAdj ++ Map(newVert1 -> Set(vertToFlip))
+          updatedAdj.transform((k, v) => if (k == newVert1) v.appended(vertToFlip) else v)
+        } else updatedAdj ++ Map(newVert1 -> Vector(vertToFlip))
 
         val newAdj2: AdjMap = if (updatedAdj.keySet.contains(newVert2)) {
-          updatedAdj.map({
-            case (newVert2, _) => (newVert2, updatedAdj(newVert2).appended(vertToFlip))
-            case x => x
-          })
-        } else updatedAdj ++ Map(newVert2 -> Set(vertToFlip))
+          updatedAdj.transform((k, v) => if (k == newVert2) v.appended(vertToFlip) else v)
+        } else updatedAdj ++ Map(newVert2 -> Vector(vertToFlip))
+
+        println(s"First case original adj: $adj")
+        println(s"First case updated adj: $updatedAdj")
+        println(s"New adj 1: $newAdj1")
+        println(s"New adj 2: $newAdj2")
 
         partitionAdjacency(newAdj1, baseVert, newVert1, newVert2) ++ partitionAdjacency(newAdj2, baseVert, newVert1, newVert2)
       }
       // Then handle the case where baseVert is pointed to
-      else if (adj.toSet.flatMap(_._2).map(_._1).contains(baseVert)) {
+      else if (adj.toSet.flatMap((x: (Vertex[Int], Vector[(Vertex[Int], Double)])) => x._2).map(_._1).contains(baseVert)) {
 
         def onceRemoved(m: AdjMap, elt: Vertex[Int]): Option[(AdjMap, Vertex[Int], (Vertex[Int], Double))] = {
           adj.find(_._2.map(_._1).contains(elt)) match {
@@ -95,24 +90,24 @@ class ModuliSpace(val g: Int, val n: Int) {
           }
         }
 
-        // TODO: Fold left with a flag and only flip one instance of baseVert to newVert1
         val (updatedAdj, sourceVertex, removedEdge): (AdjMap, Vertex[Int], (Vertex[Int], Double)) = onceRemoved(adj, baseVert).get
+        println(s"\nRemoving $baseVert once from $adj")
+        println(s"Got updated adj: $updatedAdj\nGot source of removal: $sourceVertex\nGot removed edge: $removedEdge\n")
 
         val replacementEdge1 = (newVert1, removedEdge._2)
         val replacementEdge2 = (newVert2, removedEdge._2)
 
-        val newAdj1: AdjMap = if (updatedAdj.keySet.contains(newVert1)) {
-          updatedAdj.transform((k, v) => if (k == sourceVertex) v.appended(replacementEdge1) else v)
-        } else updatedAdj ++ Map(sourceVertex -> Vector(replacementEdge1))
+        val newAdj1: AdjMap = updatedAdj.transform((k, v) => if (k == sourceVertex) v.appended(replacementEdge1) else v)
 
-        val newAdj2: AdjMap = if (updatedAdj.keySet.contains(newVert2)) {
-          updatedAdj.transform((k, v) => if (k == sourceVertex) v.appended(replacementEdge2) else v)
-        } else updatedAdj ++ Map(sourceVertex -> Vector(replacementEdge2))
+        val newAdj2: AdjMap = updatedAdj.transform((k, v) => if (k == sourceVertex) v.appended(replacementEdge2) else v)
 
         partitionAdjacency(newAdj1, baseVert, newVert1, newVert2) ++ partitionAdjacency(newAdj2, baseVert, newVert1, newVert2)
       }
       // And finally, handle the case where baseVert isn't pointed to or from
-      else Vector()
+      else {
+//        println(s"$baseVert wasn't pointed to or from")
+        Vector(adj)
+      }
     }
 
     def getSplittingSpecialization(g: UndirectedGraph[Int, Double], v: Vertex[Int], g1: Int, g2: Int,
