17b4c315a386f2de6170575fe69a48256970fabe
Connor Meredith <connor.meredith0913@gmail.com>
2020-06-01 04:26:15 -0600

    Add convenience function for removing an edge from an AdjMap

--- src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
+++ src/main/scala/tropicalcurves/modulispaces/ModuliSpace.scala
@@ -74,8 +74,30 @@ class ModuliSpace(val g: Int, val n: Int) {
       }
       // Then handle the case where baseVert is pointed to
       else if (adj.toSet.flatMap(_._2).map(_._1).contains(baseVert)) {
+
+        def onceRemoved(m: AdjMap, elt: Vertex[Int]): Option[(AdjMap, (Vertex[Int], Double))] = {
+          adj.find(_._2.map(_._1).contains(baseVert)) match {
+            case Some((keyToUpdate, valToUpdate)) => Some({
+              val adjacentEdges = m(keyToUpdate)
+              val edgeToUpdate = adjacentEdges.find(_._1 == baseVert).get
+              val indexToUpdate = adjacentEdges.indexOf(edgeToUpdate)
+
+              def removedAtIndex[A](vec: Vector[A], index: Int): Vector[A] = {
+                vec.zipWithIndex.filter(_._2 != index).map(_._1)
+              }
+
+              (m.transform((k, v) => {
+                if (k == keyToUpdate) removedAtIndex(valToUpdate, indexToUpdate)
+                else v
+              }), edgeToUpdate)
+            })
+            case None => None
+          }
+        }
+
         // TODO: Fold left with a flag and only flip one instance of baseVert to newVert1
         val newAdj1: AdjMap = adj.toVector.foldLeft[AdjMap](Map())(???)
+
 
         // TODO: Fold left with a flag and only flip one instance of baseVert to newVert2
         val newAdj2: AdjMap = adj.toVector.foldLeft[AdjMap](Map())(???)
